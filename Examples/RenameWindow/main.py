0bb978d5f5b2bd4f352046dcd6c1bae7f2dec5db
FloriaProduction <75878454+FloriaProduction@users.noreply.github.com>
2025-01-30 09:34:45 +0300

    Обновление Примера RenameWindow для v1.2

--- Examples/RenameWindow/main.py
+++ Examples/RenameWindow/main.py
@@ -1,79 +1,79 @@
 from FloriaConsoleGUI import *
-from FloriaConsoleGUI.Managers import *
+from FloriaConsoleGUI.Classes import *
+from FloriaConsoleGUI.Graphic import *
 from FloriaConsoleGUI.Graphic.Widgets import *
 from FloriaConsoleGUI.Graphic.Windows import *
-from FloriaConsoleGUI.Graphic import *
-from FloriaConsoleGUI.Classes import *
+from FloriaConsoleGUI.Managers import *
+from FloriaConsoleGUI.Graphic.Shaders import *
 
 
 @Core.initialized_all_threads_event.dec
 def _():
-    Core.setConsoleName('Example TextBox')
+    def renameWindow(window_name: str):
+        titledwindow1: TitledWindow = WindowManager.getByName('titledwindow1')
+        if titledwindow1:
+            titledwindow1.title = window_name
     
     SoundManager.load('./data/sounds/sounds.json')
-
+    KeyboardManager.pressed_event.add(
+        lambda: SoundManager.play('key_down')
+    )
+    
     WindowManager.openNewWindow(
         TitledWindow(
-            name='main',
-            offset_pos=[1, 0, 0],
-            frame=True,
-            title=' Example CheckBox ',
+            name='titledwindow1',
+            title='Example RenameWindow',
             title_anchor=Anchor.center,
-            padding=[1, 1, 1, 1],
-            direction=Orientation.vertical,
-            gap=1,
             title_style=1,
+            offset_pos=[1, 0, 0],
+            padding=[1, 1, 1, 1],
+            min_size=[50, None],
+            size_by_objects=True,
+            objects_direction=Orientation.vertical,
+            gap=1,
             widgets=[
-                Container(
-                    direction=Orientation.horizontal,
-                    gap=1,
-                    widgets=[
-                        Label(
-                            text='name:'
-                        ),
-                        TextBox(
-                            name='Textbox_name',
-                            text_max_size=[15, 1]
-                        )
-                    ]
+                TextBox(
+                    name='textbox1',
+                    
+                    placeholder='Window name',
+                    max_size=[52 - 1, 1],
                 ),
                 Button(
-                    name='Button_rename',
+                    name='button1',
+                    
                     text='Rename',
-                    clear_pixel=Pixels.black_white,
-                    select_clear_pixel=Pixels.b_blue,
                     padding=[1, 1, 1, 1],
-                    min_size=[20, None]
+                    size_hint=[1, None]
                 ),
                 Button(
-                    text='Close',
-                    clear_pixel=Pixels.black_white,
-                    select_clear_pixel=Pixels.b_blue,
+                    name='button2',
+                    
+                    text='close',
                     padding=[1, 1, 1, 1],
-                    min_size=[20, None],
-                    on_press_functions=[
-                        Core.stop
-                    ]
+                    size_hint=[1, None]
                 )
             ]
         )
     )
     
-    window_main: TitledWindow = WindowManager.getByName('main')
-    textbox: TextBox = Widget.getByName('Textbox_name')
-    button_rename: Button = Widget.getByName('Button_rename')
+    textbox1: TextBox = Widget.getByName('textbox1')
+    button1: Button = Widget.getByName('button1')
+    button2: Button = Widget.getByName('button2')
     
-    @button_rename.pressed_event.dec
-    def _():
-        window_main.title = textbox.text
+    textbox1.press_enter_event.add(
+        lambda: renameWindow(textbox1.text)
+    )
+    button1.press_enter_event.add(
+        lambda: renameWindow(textbox1.text)
+    )
+    button2.press_enter_event.add(
+        Core.stop
+    )
+    
 
-
-@KeyboardManager.pressed_event.dec
-def _():
-    SoundManager.play('key_down')
-
-
-if __name__ == "__main__":
-    Core.init()
-    Core.start()
+if __name__ == '__main__':
+    Core.init(
+        change_current_directory=__file__
+    )
+    Core.run()
     Core.term()
